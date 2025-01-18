0aa6abe9095b58d27b9e0b77db0e14db0ed1c2c2
Ali-Ahmed-Elkady <aliahmed249911@gmail.com>
2025-01-18 12:08:19 +0200

    Add files via upload

---
+++ Linq/Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public static class Extension
+    {
+        public static void Ali(this List<int> list , Func<int ,bool> fun)
+        {
+            
+            foreach (int x in list)
+            {
+                if (fun.Invoke(x))
+                Console.WriteLine(x);
+                
+            }
+        }
+        }
+    } 
---
+++ Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Linq
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1 ,2, 3 ,5,7,9,10,11,12};
+            list.Ali(i => i % 2 == 0);
+            Console.WriteLine("Hello, World!");
+        }
+       
+    } }
